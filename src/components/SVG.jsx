2f670c2202811b3ee7ad5c794ace8065cb807863
gylman <gylmanbm@gmail.com>
2023-12-18 20:25:04 +0900

    pre src, dest removal

--- src/components/SVG.jsx
+++ src/components/SVG.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Node } from './Node';
 
 const x = 0;
@@ -8,18 +8,16 @@ const 𝚫y = 200;
 const r = 40;
 const fill = 'gray';
 
-const params = [
-  {
+const params = {
+  user: {
     id: 'user',
     x: x,
     y: y + 2 * 𝚫y,
     r: r,
-    cx: x + r,
-    cy: y + r,
     fill: fill,
     text: 'u',
   },
-  {
+  s1: {
     id: 's1',
     x: x + 𝚫x,
     y: y,
@@ -27,7 +25,7 @@ const params = [
     fill: fill,
     text: 's1',
   },
-  {
+  s2: {
     id: 's2',
     x: x + 𝚫x,
     y: y + 𝚫y,
@@ -35,7 +33,7 @@ const params = [
     fill: fill,
     text: 's2',
   },
-  {
+  leader: {
     id: 'leader',
     x: x + 2 * 𝚫x,
     y: y + 2 * 𝚫y,
@@ -43,7 +41,7 @@ const params = [
     fill: fill,
     text: 'leader',
   },
-  {
+  s3: {
     id: 's3',
     x: x + 𝚫x,
     y: y + 3 * 𝚫y,
@@ -51,7 +49,7 @@ const params = [
     fill: fill,
     text: 's3',
   },
-  {
+  s4: {
     id: 's4',
     x: x + 𝚫x,
     y: y + 4 * 𝚫y,
@@ -59,74 +57,84 @@ const params = [
     fill: fill,
     text: 's4',
   },
-  {
+  l1: {
     id: 'l1',
-    defaultTimeout: 20,
     x: x + 3 * 𝚫x,
     y: y + 2 * 𝚫y,
     r: r,
     fill: fill,
     text: 'l1',
   },
+};
+
+const logs = [
+  { from: 'user', to: 's1' },
+  { from: 's1', to: 'leader' },
+  { from: 'leader', to: 's4' },
+  { from: 'leader', to: 's1' },
+  { from: 's1', to: 'user' },
+  { from: 'leader', to: 'l1' },
+  { from: 'user', to: 'leader' },
+  { from: 'leader', to: 's4' },
+  { from: 'leader', to: 'user' },
 ];
-params;
 
 const SVG = ({ txSentTo }) => {
-  const [source1] = useState({ cx: params[0].x + r, cy: params[0].y + r });
-  const [destination1] = useState({ cx: params[1].x + r, cy: params[1].y + r });
-  const [destination2] = useState({ cx: params[3].x + r, cy: params[3].y + r });
-  const [destination3] = useState({ cx: params[6].x + r, cy: params[6].y + r });
-  const pathData1 = `M ${source1.cx},${source1.cy} ${destination1.cx},${destination1.cy}`;
-  const pathData2 = `M ${destination1.cx},${destination1.cy} ${destination2.cx},${destination2.cy}`;
-  const pathData3 = `M ${destination2.cx},${destination2.cy} ${destination3.cx},${destination3.cy}`;
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [dynSrc, setDynSrc] = useState({
+    cx: params[logs[0].from].x + r,
+    cy: params[logs[0].from].y + r,
+  });
+  const [dynDest, setDynDest] = useState({
+    cx: params[logs[0].to].x + r,
+    cy: params[logs[0].to].y + r,
+  });
+
+  const [dynPathData, setDynPathData] = useState(
+    `M ${dynSrc.cx},${dynSrc.cy} ${dynDest.cx},${dynDest.cy}`
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => {
+        const nextIndex = (prevIndex + 1) % logs.length;
+        setDynSrc({
+          cx: params[logs[nextIndex].from].x + r,
+          cy: params[logs[nextIndex].from].y + r,
+        });
+        setDynDest({
+          cx: params[logs[nextIndex].to].x + r,
+          cy: params[logs[nextIndex].to].y + r,
+        });
+        setDynPathData(
+          `M ${dynSrc.cx},${dynSrc.cy}  ${dynDest.cx},${dynDest.cy}`
+        );
+        console.log(dynPathData);
+        return nextIndex;
+      });
+    }, 500); // Change the interval as needed
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const animationKey = `path-${dynSrc.cx}-${dynSrc.cy}-${dynDest.cx}-${dynDest.cy}`;
 
   return (
     <>
       <svg width='100%' height='80%' xmlns='http://www.w3.org/2000/svg'>
-        <path
-          id='transactionPath1'
-          d={pathData1}
-          fill='none'
-          stroke='none'
-          strokeWidth='2'
-        />
-        <path
-          id='transactionPath2'
-          d={pathData2}
-          fill='none'
-          stroke='none'
-          strokeWidth='2'
-        />
-        <path
-          id='transactionPath3'
-          d={pathData3}
-          fill='none'
-          stroke='none'
-          strokeWidth='2'
-        />
+        <path id={animationKey} d={dynPathData} fill='none' strokeWidth='2' />
         <circle r='5' fill='red'>
-          <animateMotion dur='0.5s' repeatCount='indefinite'>
-            <mpath href='#transactionPath1' />
+          <animateMotion dur='0.5s'>
+            <mpath href={`#${animationKey}`} />
           </animateMotion>
         </circle>
-        <circle r='5' fill='red'>
-          <animateMotion dur='0.5s' repeatCount='indefinite'>
-            <mpath href='#transactionPath2' />
-          </animateMotion>
-        </circle>
-        <circle r='5' fill='red'>
-          <animateMotion dur='0.5s' repeatCount='indefinite'>
-            <mpath href='#transactionPath3' />
-          </animateMotion>
-        </circle>
-
-        <Node {...params[0]} />
-        <Node {...params[1]} />
-        <Node {...params[2]} />
-        <Node {...params[3]} />
-        <Node {...params[4]} />
-        <Node {...params[5]} />
-        <Node {...params[6]} />
+        <Node {...params.user} />
+        <Node {...params.s1} />
+        <Node {...params.s2} />
+        <Node {...params.leader} />
+        <Node {...params.s3} />
+        <Node {...params.s4} />
+        <Node {...params.l1} />
       </svg>
     </>
   );
