9576c2ae5853004aa28ce4b6cb2d4bac75005e70
gylman <gylmanbm@gmail.com>
2024-01-23 14:04:24 +0900

    refactor circle component

--- src/components/Circle.jsx
+++ src/components/Circle.jsx
@@ -3,15 +3,14 @@ import React from 'react';
 const Circle = ({ color, motionPath, duration, isReversed }) => {
   return (
     <circle r='5' fill={color}>
-      {isReversed ? (
-        <animateMotion dur={`${duration}ms`} repeatCount='indefinite' keyPoints='1;0' keyTimes='0;1'>
-          <mpath href={`#${motionPath}`} />
-        </animateMotion>
-      ) : (
-        <animateMotion dur={`${duration}ms`} repeatCount='indefinite'>
-          <mpath href={`#${motionPath}`} />
-        </animateMotion>
-      )}
+      <animateMotion
+        dur={`${duration}ms`}
+        repeatCount='indefinite'
+        keyPoints={isReversed ? '1;0' : '0;1'}
+        keyTimes='0;1'
+      >
+        <mpath href={`#${motionPath}`} />
+      </animateMotion>
     </circle>
   );
 };
